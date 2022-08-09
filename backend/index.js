8f3451045a103a17080100b74d46f1696bb35af7
i-zokirov <izokirov@outlook.com>
2022-08-09 19:51:17 +0200

    backend fix

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ import {
     handleMessage,
     fetchVideoChatData,
 } from "./controllers/socket/messageController.js";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 connectDB();
 
